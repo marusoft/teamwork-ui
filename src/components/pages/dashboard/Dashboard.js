ce50e6e4533dc5fe9bce2aa2f49ad12d5f82895c
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2021-09-04 18:04:29 +0100

    add sidebar features

--- src/components/pages/dashboard/Dashboard.js
+++ src/components/pages/dashboard/Dashboard.js
@@ -1,12 +1,56 @@
-import React from "react";
-import "./dasboard.css"
+import React, { useState, useEffect } from "react";
+import { loggedInUrl } from "../../apis";
+import { getToken } from "../../auth";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useHistory } from "react-router-dom";
+
+import "./dasboard.css";
 
 const Dashboard = () => {
+  const history = useHistory();
+
+  const [name, setName] = useState("");
+
+  const getLoggedInUser = async () => {
+    const token = getToken();
+    try {
+      const loggedInUser = await fetch(loggedInUrl, {
+        method: "GET",
+        headers: { Authorization: token },
+      });
+      const loggedUserResult = await loggedInUser.json();
+      const { username } = loggedUserResult.data;
+      setName(username);
+      toast.success("Welcome back your login was successful");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const logOut = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    history.push("/login");
+  };
+
+  useEffect(() => {
+    getLoggedInUser();
+  }, []);
+
   return (
-    <div className="dashboard">
-      hello dashboard
-    </div>
-  )
-}
+    <>
+      <ToastContainer />
+      <div className="dashboard">
+        <div className="dashboard-welcome">
+          <h3>Hello</h3>
+          <span>{name}</span>
+        </div>
+        <button onClick={(e) => logOut(e)} className="logout-btn">Sign Out</button>
+      </div>
+    </>
+  );
+};
 
 export default Dashboard;
